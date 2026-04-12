05a348a488036cf29503db2159a15ade492bb50e
MonoSama21 <yrvin21pachas2004@gmail.com>
2026-04-12 00:11:40 -0500

    V.01 - Sobre esta piedra edificaré mi iglesia - Yrvin Pachas

--- src/main/docs/find_sequence_name.sql
+++
@@ -1,9 +0,0 @@
--- SCRIPT PARA ENCONTRAR EL NOMBRE CORRECTO DE LA SECUENCIA
--- Ejecuta esto primero en Supabase SQL Editor
-
--- En el schema PRODUCCION
-SELECT * FROM pg_sequences 
-WHERE tablename = 'institucioneseducativas' OR schemaname = 'produccion';
-
--- Esto mostrará todas las secuencias disponibles
--- Busca la que corresponda a institucioneseducativas y copia su nombre exacto
